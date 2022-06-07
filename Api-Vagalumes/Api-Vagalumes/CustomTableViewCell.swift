9da59f9c8b8ecea6cb6ee105e0c61867e4b6cf6d
Weberth Erik Anolar Sirqueira <51481418+Weberth1233@users.noreply.github.com>
2022-06-07 11:30:55 -0300

    Alterações
    add novos arquivos

---
+++ Api-Vagalumes/Api-Vagalumes/CustomTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTableViewCell.swift
+//  Api-Vagalumes
+//
+//  Created by pat002900 on 31/05/22.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+
+    
+    
+}
---
+++ Api-Vagalumes/Api-Vagalumes/NewsCell.swift
@@ -0,0 +1,13 @@
+//
+//  NewsCell.swift
+//  Api-Vagalumes
+//
+//  Created by pat002900 on 07/06/22.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+    @IBOutlet weak var imageApi: UIImageView!
+    @IBOutlet weak var textApi: UILabel!
+}
